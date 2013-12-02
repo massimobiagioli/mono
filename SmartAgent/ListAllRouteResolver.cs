876662daa9dd01fa1606b30bcc4902e6804f3a3f
massimobiagioli <github@massimobiagioli.it>
2013-12-02 20:44:55 +0100

    Implementata route ListAll

--- SmartAgent/ListAllRouteResolver.cs
+++ SmartAgent/ListAllRouteResolver.cs
@@ -1,18 +1,25 @@
 using System;
 using System.Net;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SmartAgent
 {
 	public class ListAllRouteResolver : IRouteResolver
 	{
+		private const string DOC_FILTER = "*.html";
+
 		public ListAllRouteResolver()
 		{
 		}
 
 		public RouteResolverResponse SendResponse(HttpListenerContext context)
 		{
-			return new RouteResolverResponse((int)HttpStatusCode.OK, JsonConvert.SerializeObject("ok"));
+			String path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			List<string> filez = new List<string>(Directory.EnumerateFiles(path, DOC_FILTER));
+
+			return new RouteResolverResponse((int)HttpStatusCode.OK, JsonConvert.SerializeObject(filez));
 		}
 	}
 }
